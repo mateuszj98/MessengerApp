ce68bc3901e3ea5ad51985499e3cc8e40ea9ca27
Hubert Chudek <hubert.chudek@gmail.com>
2021-09-15 23:18:51 +0200

    Working POST new user on registration.

--- app/src/main/java/com/example/messenger/RegisterActivity.java
+++ app/src/main/java/com/example/messenger/RegisterActivity.java
@@ -11,6 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -18,6 +22,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -31,6 +38,8 @@ public class RegisterActivity extends AppCompatActivity {
     FirebaseAuth auth;
     DatabaseReference dbRef;
 
+    RequestQueue requestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +52,7 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
 
         auth = FirebaseAuth.getInstance();
+        requestQueue = Volley.newRequestQueue(this);
         //Adding Event Listener to Register Button
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,20 +61,20 @@ public class RegisterActivity extends AppCompatActivity {
                 String passwordText = passwordEditText.getText().toString();
                 String emailText = emailEditText.getText().toString();
 
-                if(TextUtils.isEmpty(usernameText) || TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passwordText)){
-                    Toast.makeText(RegisterActivity.this,"Please fill all fields!", Toast.LENGTH_LONG).show();
-                }else{
+                if (TextUtils.isEmpty(usernameText) || TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passwordText)) {
+                    Toast.makeText(RegisterActivity.this, "Please fill all fields!", Toast.LENGTH_LONG).show();
+                } else {
                     register(usernameText, passwordText, emailText);
                 }
             }
         });
     }
 
-    private void register(final String username, String password, String email){
+    private void register(final String username, String password, String email) {
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     FirebaseUser firebaseUser = auth.getCurrentUser();
                     assert firebaseUser != null;
                     String userid = firebaseUser.getUid();
@@ -72,27 +82,57 @@ public class RegisterActivity extends AppCompatActivity {
                     dbRef = FirebaseDatabase.getInstance().getReference("MyUsers").child(userid);
 
                     //HashMaps
-                    HashMap<String,String> hashMap = new HashMap<>();
+                    HashMap<String, String> hashMap = new HashMap<>();
                     hashMap.put("id", userid);
                     hashMap.put("username", username);
                     hashMap.put("imageURL", "default");
+
+                    //JSON object for API
+                    JSONObject jsonObject = new JSONObject();
+                    try {
+                        //jsonObject.put("id", userid);
+                        jsonObject.put("name", username);
+                        //jsonObject.put("imageURL", "default");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    //Send data to API
+                    volleyPost(MainActivity.API_URL + "/api/user", jsonObject);
+                    //makeIntent(userid);
 
                     //Opening the Main Activity after successful registration
                     dbRef.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()){
-                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(intent);
-                                finish();
+                            if (task.isSuccessful()) {
+                                makeIntent(userid);
                             }
                         }
                     });
-                }else{
-                    Toast.makeText(RegisterActivity.this,"Invalid Email or Password",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Invalid Email or Password", Toast.LENGTH_SHORT).show();
                 }
             }
+
+            private void makeIntent(String userUid) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("user.name", username);
+                intent.putExtra("user.uid", userUid);
+                startActivity(intent);
+                finish();
+            }
         });
+    }
+
+    private void volleyPost(String postUrl, JSONObject postData) {
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, postData,
+                response -> System.out.println(response),
+                error -> error.printStackTrace());
+
+        requestQueue.add(jsonObjectRequest);
+
     }
 }
